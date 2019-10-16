e5833597889299a429b66fc18eb5f3a6875f74cd
LuLCotrim <lucaslcotrim@hotmail.com>
2019-10-16 15:03:05 -0300

    Continue development of the js file

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-const addButton = document.querySelector('button.add')
+const addButton = document.getElementById('add')
+console.log(addButton)
+const textBox = document.getElementById('descTodo')
+const listaTodo = document.getElementsByClassName('list')
 
-const addTodo = () => {
-    const 
+function addTodo(){
+    const li = document.createElement('li')
+    const bt = document.createElement('button')
+    const icon = document.createElement('i')
+    let textTodo = documnet.createTextNode(textBox.value)
+
+    icon.setAttribute('class', 'fas fa-chevron-circle-right')
+    icon.setAttribute('style', 'font-size: 18px')
+    
+    li.setAttribute('class', 'listElement')
+
+    bt.appendChild(icon)
+
+    li.appendChild(bt)
+    li.appendChild(textTodo)
+
+    listaTodo.appendChild(li)
 }
 
-addButton.onclick = addTodo
+addButton.onclick = alert('dfdfd')
