748a2809da8a17725ab8a28def3ad321e9f1c26c
LuLCotrim <lucaslcotrim@hotmail.com>
2019-10-18 15:38:25 -0300

    finished the renderLists function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const addButton = document.getElementById('add')
 const textBox = document.getElementById('descTodo')
-const listaTodo = document.getElementById('list')
+const listaTodo = document.getElementById('listTodo')
+const listaDoing = document.getElementById('listDoing')
+const listaDone = document.getElementById('listDone')
 
 let todoArray = []
 let doingArray = []
-let donerray = []
+let doneArray = []
 
 function addTodo(){
     todoArray.push(textBox.value)
@@ -14,6 +16,9 @@ function addTodo(){
 
 const renderLists = () => {
     listaTodo.innerHTML = null
+    listaDoing.innerHTML = null
+    listaDone.innerHTML = null
+
     todoArray.forEach(element => {
         const li = document.createElement('li')
         const bt = document.createElement('button')
@@ -32,6 +37,45 @@ const renderLists = () => {
 
         listaTodo.appendChild(li)
     });
+
+    doingArray.forEach(element => {
+        const li = document.createElement('li')
+        const bt = document.createElement('button')
+        const icon = document.createElement('i')
+        let textTodo = document.createTextNode(element)
+
+        icon.setAttribute('class', 'fas fa-chevron-circle-right')
+        icon.setAttribute('style', 'font-size: 18px')
+    
+        li.setAttribute('class', 'listElement')
+
+        bt.appendChild(icon)
+
+        li.appendChild(textTodo)
+        li.appendChild(bt)
+
+        listaDoing.appendChild(li)
+    })
+
+    doneArray.forEach(element => {
+        const li = document.createElement('li')
+        const bt = document.createElement('button')
+        const icon = document.createElement('i')
+        let textTodo = document.createTextNode(element)
+
+        icon.setAttribute('class', 'fas fa-times-circle')
+        icon.setAttribute('style', 'font-size: 18px')
+    
+        li.setAttribute('class', 'listElement')
+
+        bt.appendChild(icon)
+
+        li.appendChild(textTodo)
+        li.appendChild(bt)
+
+        listaDoing.appendChild(li)
+    })
 }
 
+renderLists()
 addButton.onclick = addTodo
